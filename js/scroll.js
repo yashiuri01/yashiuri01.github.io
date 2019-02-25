3f942a019bc1e16567871e0a9d99d66586736580
yashiuri <yashiuri01@gmail.com>
2019-02-25 22:53:13 +0900

    bootstrap

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,15 @@ $(function(){
     var speed = 500;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+    var headerHeight = 55;
+    var position = target.offset().top - headerHeight;
+    $("html, body").animate({scrollTop:position},  speed, "swing");
     return false;
+  });
+});
+
+$(document).ready(function (){
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
   });
 });
