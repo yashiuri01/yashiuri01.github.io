b1e835f34b82e225eeab26c5927e073e007abb56
Yuuri Takebayashi <yuuri.takebayashi@ibm.com>
2019-02-26 18:03:55 +0900

    responsive

--- js/scroll.js
+++ js/scroll.js
@@ -12,14 +12,35 @@ $(function(){
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
+
+  var ua = navigator.userAgent;
+  if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+    //ウィンドウの高さを取得する
+    var targetY = $(window).height();
+    //スクロールをクリックするとウィンドウの高さ分、下にスクロールする
+    $('.js-scroll a').on('click',function(){
+      $("html, body").stop().animate({scrollTop: targetY}, 750, 'swing');
+      return false;
+    });
+  } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        // タブレット用コード
+  } else {
+    //ウィンドウの高さを取得する
+    var targetY = $(window).height() - 55;
+    //スクロールをクリックするとウィンドウの高さ分、下にスクロールする
+    $('.js-scroll a').on('click',function(){
+      $("html, body").stop().animate({scrollTop: targetY}, 750, 'swing');
+      return false;
+    });
+  }
 });
 
-$(window).on('load resize',function(){
-  //ウィンドウの高さを取得する
-  var targetY = $(window).height() - 55;
-  //スクロールをクリックするとウィンドウの高さ分、下にスクロールする
-  $('.js-scroll a').on('click',function(){
-    $("html, body").stop().animate({scrollTop: targetY}, 750, 'swing');
-    return false;
-  });
-});
+// $(window).on('load resize',function(){
+//     //ウィンドウの高さを取得する
+//     var targetY = $(window).height();
+//     //スクロールをクリックするとウィンドウの高さ分、下にスクロールする
+//     $('.js-scroll a').on('click',function(){
+//       $("html, body").stop().animate({scrollTop: targetY}, 750, 'swing');
+//       return false;
+//     });
+// });
