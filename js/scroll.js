36de70bafefda4d6f3f1d0dc441296a999c68d16
Yuuri Takebayashi <yuuri.takebayashi@ibm.com>
2019-02-26 17:20:07 +0900

    This commit fix #4, fix#5, fix#7

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 $(function(){
   $('a[href^="#"]').click(function(){
-    var speed = 500;
+    var speed = 750;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var headerHeight = 55;
@@ -8,10 +8,18 @@ $(function(){
     $("html, body").animate({scrollTop:position},  speed, "swing");
     return false;
   });
-});
 
-$(document).ready(function (){
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
+  });
+});
+
+$(window).on('load resize',function(){
+  //ウィンドウの高さを取得する
+  var targetY = $(window).height() - 55;
+  //スクロールをクリックするとウィンドウの高さ分、下にスクロールする
+  $('.js-scroll a').on('click',function(){
+    $("html, body").stop().animate({scrollTop: targetY}, 750, 'swing');
+    return false;
   });
 });
